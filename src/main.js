3c0329154fb6f79ca99bce0145dc636b31b51310
Maciej Korsan <maciek@cotenfrontend.pl>
2023-08-21 23:19:57 +0200

    scripts for handling the localstorage

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const textarea = document.querySelector('.textarea--js');
+const loadButton = document.querySelector('.load--js');
+const saveButton = document.querySelector('.save--js');
+
+saveButton.addEventListener('click', () => {
+  localStorage.setItem('content', textarea.value)
+})
+
+loadButton.addEventListener('click', () => {
+  textarea.value = localStorage.getItem('content');
+})
